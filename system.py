0d88b4d02430fdaf32cdd2a547090e001efc0aa3
Felipe Cortez <felipecortezfi@gmail.com>
2019-04-28 16:13:56 -0300

    Fix CPU usage and move initialization to a function
    How? No idea.

--- system.py
+++ system.py
@@ -2,96 +2,99 @@ import sys, pygame, midi, cairo
 from theory import *
 from draw import *
 
-size = width, height = 512, 512
-
-pygame.init()
-pygame.display.set_mode(size)
-
-#clock = pygame.time.Clock()
-#clock.tick(30)
-
-screen = pygame.display.get_surface()
-
-player = midi.MidiPlayer()
-
-bases = cycle([ionian, natural_minor, harmonic_minor, melodic_minor])
-base = next(bases)
-
-music_key = 60
-
-key_to_chord = {
-    pygame.K_a: "i",
-    pygame.K_s: "iv",
-    pygame.K_d: "v",
-
-    pygame.K_z: "ii",
-    pygame.K_x: "iii",
-    pygame.K_c: "vi",
-
-    pygame.K_v: "vii",
-}
-
-active_keys = {}
-
 def pressed(keycode):
     return pygame.key.get_pressed()[keycode]
 
-screen.fill((0, 0, 0))
-pygame.display.flip()
+def main():
+    size = width, height = 512, 512
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_TAB:
-                for i in range(130):
-                    player.noteoff(i)
+    player = midi.MidiPlayer()
 
-            if event.key == pygame.K_UP:
-                music_key += 1
+    bases = cycle([ionian, natural_minor, harmonic_minor, melodic_minor])
+    base = next(bases)
 
-            if event.key == pygame.K_DOWN:
-                music_key -= 1
+    music_key = 60
 
-            if event.key == pygame.K_RIGHT:
-                base = next(bases)
+    key_to_chord = {
+        pygame.K_a: "i",
+        pygame.K_s: "iv",
+        pygame.K_d: "v",
 
-            if event.key in key_to_chord.keys():
-                chordfn = triad if not pressed(pygame.K_j) else tetrad
-                chord = deg(base, key_to_chord[event.key], music_key, chordfn)
-                root = chord[0]
+        pygame.K_z: "ii",
+        pygame.K_x: "iii",
+        pygame.K_c: "vi",
 
-                inv_keys = [pygame.K_p, pygame.K_o, pygame.K_i]
-                for _ in [k for k in inv_keys if pressed(k)]:
-                    chord = invert_down(chord)
+        pygame.K_v: "vii",
+    }
 
-                if pressed(pygame.K_SPACE):
-                    chord = bass(chord, root)
+    active_keys = {}
 
-                if pressed(pygame.K_n):
-                    chord = bass_fifth(chord, root)
+    pygame.init()
+    pygame.display.set_mode(size)
+    screen = pygame.display.get_surface()
 
-                print(chord)
+    screen.fill((0, 0, 0))
 
-                active_keys[event.key] = chord
+    clock = pygame.time.Clock()
 
-                for note in chord:
-                    player.noteon(note)
+    done = 0
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = 1
 
-            if event.key == pygame.K_t:
-                surface = draw()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_TAB:
+                    for i in range(130):
+                        player.noteoff(i)
 
-                buf = surface.get_data()
-                image = pygame.image.frombuffer(buf, (width, height), "ARGB")
+                if event.key == pygame.K_UP:
+                    music_key += 1
 
-                screen.fill((0, 0, 0))
-                screen.blit(image, (0, 0))
-                pygame.display.flip()
+                if event.key == pygame.K_DOWN:
+                    music_key -= 1
 
-        if event.type == pygame.KEYUP:
-            if event.key in key_to_chord.keys():
-                for note in active_keys.pop(event.key):
-                    player.noteoff(note)
+                if event.key == pygame.K_RIGHT:
+                    base = next(bases)
 
-        if event.type == pygame.QUIT:
-            sys.exit()
+                if event.key in key_to_chord.keys():
+                    chordfn = triad if not pressed(pygame.K_j) else tetrad
+                    chord = deg(base, key_to_chord[event.key], music_key, chordfn)
+                    root = chord[0]
 
+                    inv_keys = [pygame.K_p, pygame.K_o, pygame.K_i]
+                    for _ in [k for k in inv_keys if pressed(k)]:
+                        chord = invert_down(chord)
+
+                    if pressed(pygame.K_SPACE):
+                        chord = bass(chord, root)
+
+                    if pressed(pygame.K_n):
+                        chord = bass_fifth(chord, root)
+
+                    print(chord)
+
+                    active_keys[event.key] = chord
+
+                    for note in chord:
+                        player.noteon(note)
+
+                if event.key == pygame.K_t:
+                    surface = draw()
+
+                    buf = surface.get_data()
+                    image = pygame.image.frombuffer(buf, (width, height), "ARGB")
+
+                    screen.fill((0, 0, 0))
+                    screen.blit(image, (0, 0))
+                    pygame.display.flip()
+
+            if event.type == pygame.KEYUP:
+                if event.key in key_to_chord.keys():
+                    for note in active_keys.pop(event.key):
+                        player.noteoff(note)
+
+        clock.tick(50)
+
+if __name__ == '__main__':
+    main()
